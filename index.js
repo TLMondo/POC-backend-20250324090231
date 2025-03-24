613b7edb6eb632af9c05c4bf537e1647b326ad82
TLMondo <armando.rosario@tealium.com>
2025-03-24 09:02:36 +0000

    Edit backend - scenario step 2

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const express = require('express'); const app = express(); app.get('/', (req,res)=>res.send('Hello POC Backend!')); app.listen(3000);
 console.log('Edit 1 - Backend');
+console.log('Edit 2 - Backend');
